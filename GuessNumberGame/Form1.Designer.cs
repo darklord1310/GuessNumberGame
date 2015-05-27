9ce1f8bbd2a4a79b3b0a76cc9ff8a683d73d23c0
darklord1310 <jet_wong@hotmail.com>
2015-05-27 23:49:15 +0800

    initial commit

---
+++ GuessNumberGame/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace GuessNumberGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxMsg = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuessNumber = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.gbxMsg.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxMsg
+            // 
+            this.gbxMsg.Controls.Add(this.label3);
+            this.gbxMsg.Controls.Add(this.label2);
+            this.gbxMsg.Controls.Add(this.txtMsg);
+            this.gbxMsg.Location = new System.Drawing.Point(15, 248);
+            this.gbxMsg.Name = "gbxMsg";
+            this.gbxMsg.Size = new System.Drawing.Size(277, 85);
+            this.gbxMsg.TabIndex = 0;
+            this.gbxMsg.TabStop = false;
+            this.gbxMsg.Text = "Feedback";
+            this.gbxMsg.Enter += new System.EventHandler(this.gbxMsg_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(23, 37);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.Size = new System.Drawing.Size(235, 20);
+            this.txtMsg.TabIndex = 0;
+            this.txtMsg.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Guess :  ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtGuessNumber
+            // 
+            this.txtGuessNumber.Location = new System.Drawing.Point(92, 6);
+            this.txtGuessNumber.Name = "txtGuessNumber";
+            this.txtGuessNumber.Size = new System.Drawing.Size(123, 20);
+            this.txtGuessNumber.TabIndex = 2;
+            this.txtGuessNumber.TextChanged += new System.EventHandler(this.txtGuessNumber_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(92, 72);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(123, 25);
+            this.btnGuess.TabIndex = 3;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 395);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuessNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbxMsg);
+            this.Name = "Form1";
+            this.Text = "Guess The Number!";
+            this.gbxMsg.ResumeLayout(false);
+            this.gbxMsg.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuessNumber;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMsg;
+    }
+}
+
---
+++ GuessNumberGame/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberGame
+{
+    public partial class Form1 : Form
+    {
+        int NumberToWin, numberoftrying = 0;            //declare here to make variable able to use in other method
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random rnd = new Random();
+            NumberToWin = rnd.Next(1, 101); // creates a number between 1 and 100
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtGuessNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            numberoftrying++;
+            int anInteger;
+            anInteger = Convert.ToInt32(txtGuessNumber.Text);
+            if (anInteger == NumberToWin)
+                txtMsg.Text = "Bingo! You Win! You used " + numberoftrying + " times";
+            else if (anInteger < NumberToWin)
+                txtMsg.Text = "The value entered is too low!";
+            else
+                txtMsg.Text = "The value entered is too high";
+        }
+
+        private void gbxMsg_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
